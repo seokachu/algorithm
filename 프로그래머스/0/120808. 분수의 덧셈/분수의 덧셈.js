c960a1e8f0f21f3713046c21d2da51c810d65d64
SeoyoungPark(seokachu) <seokachuu@gmail.com>
2024-09-18 18:08:38 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,11 @@
+function fnGCD(a, b){
+    return (a%b)? fnGCD(b, a%b) : b;
+}
+
+function solution(denum1, num1, denum2, num2) {
+    let denum = denum1*num2 + denum2*num1;
+    let num = num1 * num2;
+    let gcd = fnGCD(denum, num); //최대공약수
+
+    return [denum/gcd, num/gcd];
+}
